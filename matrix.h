562ded6dbce37010089a96124c3fb38faafe9f77
Pavan Preetham Valluri <44934630+ppvalluri09@users.noreply.github.com>
2019-03-17 22:07:10 +0530

    Add files via upload

---
+++ matrix.h
@@ -0,0 +1,304 @@
+
+// Header file for matrix operations associated with linear algebra and Neural Networks. . .
+
+#include<iostream>
+#include<stdio.h>
+#include<math.h>
+#include<stdlib.h>
+#include<iomanip>
+#include<time.h>
+#define MAX_ROWS 5000
+#define PRECISION 6
+
+using namespace std;
+
+long double sigmoid(long double x) {                      // Sigmoid Activation Function
+        return 1.0/(1.0 + (long double)exp(-x));
+}
+
+long double softplus(long double x) {                     // SoftPlus Activation Function
+        return log(1 + exp(x)); 
+}
+
+long double elu(long double x) {                          // ELU (Exponential Linear Unit) Activation Function
+        long double a = 0.1;
+        if (x > 0.0) {
+                return x;
+        } else {
+                return a * (exp(x) - 1);
+        }
+}
+
+
+class Matrix {                          // A matrix class containing the matrix, number of rows and columns. . .
+
+        private:
+                int rows;
+                int cols;
+                long double *data[MAX_ROWS];
+        public:
+                Matrix(int rows, int cols) {            // Matrix constructor with the number of rows and columns. . .
+                        this->rows = rows;
+                        this->cols = cols;
+                        for (int i=0; i < this->rows; i++) {
+                                data[i] = (long double*)malloc(this->cols * sizeof(long double));
+                                for (int j=0; j < this->cols; j++) {
+                                        data[i][j] = 0.0;
+                                }
+                        }
+                }
+
+                Matrix() {
+                        this->rows = 0;
+                        this->cols = 0;
+                }
+
+                void setSize(int, int);
+                void printSize() {
+                        printf("%d, %d\n", rows, cols);
+                }
+
+                void print();                           // Function to print the matrix...
+                void multiply(long double);                  // Scalar Multiplication...
+                void randomize();                       // Function to assign randomly generated values to the matrices ...
+                void add(long double);                       // Scalar addition...
+                void subtract(long double);                  // Scalar subtraction...
+                static Matrix transpose(Matrix);        // Static member function to find the transpose of the matrix...
+                static Matrix add(Matrix, Matrix);      // Static member function to add two matices and return the result...
+                static Matrix multiply(Matrix, Matrix); // Static member function to multiply two matices and return the result...
+                static Matrix subtract(Matrix, Matrix); // Static member function to subtract two matices and return the result...
+                void scale(long double n);                   // Function to scale the matrix, i.e multiply all elements by n...
+                void square();                          // Function to square all the elements in the matrix...
+                static Matrix map(Matrix, int);              // Function to calculate the value of the Activation Function...
+                static Matrix fromArray(long double*, int);       // Function to convert an array to an object of type "Matrix"...
+                long double* toArray();                      // Function to return the array associated with the object...
+                void dot(Matrix);
+                
+                Matrix operator + (Matrix const &m);
+                Matrix operator - (Matrix const &m);
+                Matrix operator * (Matrix const &m);
+
+                long double getValue() {
+                        return this->data[0][0];
+                }
+};
+
+Matrix Matrix::operator +(Matrix const &m)  {
+        Matrix x(this->rows, this->cols);
+        if (this->rows != m.rows || this->cols != m.cols) {
+                cout<<"Number of rows and columns must be equal!!!"<<endl;
+        } else {
+                for (int i = 0; i < x.rows; i++) {
+                        for (int j = 0; j < x.cols; j++) {
+                                x.data[i][j] = this->data[i][j] + m.data[i][j];
+                        }
+                }
+        }
+        return x;
+}
+
+Matrix Matrix::operator -(Matrix const &m)  {
+        Matrix x(this->rows, this->cols);
+        if (this->rows != m.rows || this->cols != m.cols) {
+                cout<<"Number of rows and columns must be equal!!!"<<endl;
+        } else {
+                for (int i = 0; i < x.rows; i++) {
+                        for (int j = 0; j < x.cols; j++) {
+                                x.data[i][j] = this->data[i][j] - m.data[i][j];
+                        }
+                }
+        }
+        return x;
+}
+
+
+Matrix Matrix::operator * (Matrix const &m) {
+        if (this->cols != m.rows) {
+                printf("The colums of the first matrix must be equal to the rows of the second matrix\n");
+        } else {
+                Matrix result(this->rows, m.cols);
+                for (int i=0; i<result.rows; i++) {
+                        for (int j=0; j<result.cols; j++) {
+                                result.data[i][j] = 0.0;
+                                for (int k = 0; k < this->cols; k++) {
+                                        result.data[i][j] += this->data[i][k] * m.data[k][j];
+                                }
+                        }
+                }
+                return result;
+        }
+}
+
+
+void Matrix::setSize(int rows, int cols) {
+        this->rows = rows; 
+        this->cols = cols;
+        for (int i=0; i < this->rows; i++) {
+                data[i] = (long double*)malloc(this->cols * sizeof(long double));
+                for (int j=0; j < this->cols; j++) {
+                        data[i][j] = 0.0;
+                }
+        }
+}
+
+long double* Matrix::toArray() {
+        long double *a = (long double*)malloc(sizeof(long double) * (this->rows * this->cols));
+        int k=0;
+        for (int i=0; i<rows; i++) {
+                for (int j=0; j<cols; j++) {
+                        a[k++] = data[i][j];
+                }
+        }
+        return a;
+}
+
+void Matrix::square() {
+        for (int i=0; i<rows; i++) {
+                for (int j=0; j<cols; j++) {
+                        data[i][j] = (pow(data[i][j], 2));
+                }
+        }
+}
+
+void Matrix::scale(long double n) {
+	for (int i=0; i<rows; i++) {
+		for (int j=0; j<cols; j++) {
+			data[i][j] *= n;
+		}	
+	}
+}
+
+Matrix Matrix::fromArray(long double *a, int length) {
+        Matrix result(length, 1);
+        for (int i=0; i<length; i++) {
+                result.data[i][0] = a[i];
+        }
+        return result;
+}
+
+Matrix Matrix::map(Matrix m, int mode) {
+        Matrix result(m.rows, m.cols);
+        if (mode == 1) {
+                for (int i=0; i<result.rows; i++) {
+                        for (int j=0; j<result.cols; j++) {
+                                long double val = m.data[i][j];
+                                result.data[i][j] = sigmoid(val);
+                        }
+                }
+        } else if (mode == 2) {
+                for (int i=0; i<result.rows; i++) {
+                        for (int j=0; j<result.cols; j++) {
+                                long double val = m.data[i][j];
+                                result.data[i][j] = sigmoid(val) * (1 - sigmoid(val));
+                        }
+                }
+        }
+        return result;
+}
+
+Matrix Matrix::multiply(Matrix m1, Matrix m2) {
+        if (m1.cols != m2.rows) {
+                printf("The colums of the first matrix must be equal to the rows of the second matrix\n");
+        } else {
+                Matrix result(m1.rows, m2.cols);
+                for (int i=0; i<result.rows; i++) {
+                        for (int j=0; j<result.cols; j++) {
+                                result.data[i][j] = 0.0;
+                                for (int k=0; k<m1.cols; k++) {
+                                        result.data[i][j] += m1.data[i][k] * m2.data[k][j];
+                                }
+                        }
+                }
+                return result;
+        }
+}
+
+Matrix Matrix::transpose(Matrix m) {
+        Matrix result(m.cols, m.rows);
+        for (int i=0; i<result.rows; i++) {
+                for (int j=0; j<result.cols; j++) {
+                        result.data[i][j] = m.data[j][i];
+                }
+        }
+        return result;
+}
+
+Matrix Matrix::subtract(Matrix m1, Matrix m2) {
+        if (m1.rows != m2.rows || m1.cols != m2.cols) {
+                printf("Number of Rows and Columns must be equal in both matrices\n");
+        } else {
+                Matrix result(m1.rows, m1.cols);
+                for (int i=0; i<result.rows; i++) {
+                        for (int j=0; j<result.cols; j++) {
+                                result.data[i][j] = m1.data[i][j] - m2.data[i][j];
+                        }
+                }
+                return result;
+        }
+}
+
+Matrix Matrix::add(Matrix m1, Matrix m2) {
+        if (m1.rows != m2.rows || m1.cols != m2.cols) {
+                printf("Number of Rows and Columns must be equal in both matrices\n");
+        } else {
+                Matrix result(m1.rows, m1.cols);
+                for (int i=0; i<result.rows; i++) {
+                        for (int j=0; j<result.cols; j++) {
+                                result.data[i][j] = m1.data[i][j] + m2.data[i][j];
+                        }
+                }
+                return result;
+        }
+}
+
+void Matrix::print() {
+        for (int i=0; i < rows; i++) {
+                for (int j=0; j < cols; j++) {
+                        cout<<fixed<<showpoint;
+                        cout<<setprecision(PRECISION);                  // Setting the precision of data
+                        cout<<data[i][j]<<"  ";
+                }
+                cout<<"\n";
+        }
+        cout<<endl;
+}
+
+void Matrix::multiply(long double n) {
+        for (int i=0; i<rows; i++) {
+                for (int j=0; j<cols; j++) {
+                        data[i][j] *= n;
+                }
+        }
+}
+
+void Matrix::randomize() {                      // Randomize generates the random values between 0 and 1. . .
+        for (int i=0; i<rows; i++) {
+                for (int j=0; j<cols; j++) {
+                        data[i][j] = ((long double) rand() / (RAND_MAX));
+                }
+        }
+}
+
+void Matrix::add(long double n) {
+        for (int i=0; i<rows; i++) {
+                for (int j=0; j<cols; j++) {
+                        data[i][j] += n;
+                }
+        }
+}
+
+void Matrix::subtract(long double n) {
+        for (int i=0; i<rows; i++) {
+                for (int j=0; j<cols; j++) {
+                        data[i][j] -= n;
+                }
+        }
+}
+
+void Matrix::dot(Matrix m) {
+        for (int i = 0; i < this->rows; i++) {
+                for (int j = 0; j < this->cols; j++) {
+                        this->data[i][j] = this->data[i][j] * m.data[i][j];
+                }
+        }
+}
---
+++ nn.h
@@ -0,0 +1,113 @@
+
+// Neural Network header file for training and classification of data. . .
+
+#include<stdio.h>
+#include<iostream>
+#include<stdlib.h>
+#include<time.h>
+
+using namespace std;
+
+class NeuralNetwork {			// Neural Network class...
+	
+	private:
+		int input_nodes;		// Input Layer...
+		int hidden_nodes;		// Hidden Layer...
+		int output_nodes;		// Output Layer...
+		long double learning_rate;		// Learning Rate...
+		Matrix weights_ih;		// Weights from input to hidden layer...
+		Matrix weights_ho;		// Weights from hidden to output layer...
+		Matrix bias_h;			// Hidden Bias...
+		Matrix bias_o;			// Output Bias...
+
+
+	public:
+
+		NeuralNetwork(int input_nodes, int hidden_nodes, int output_nodes) {		// Constructor to get number of input, hidden, output nodes to generate weight and bias matrices. . .
+
+			this->input_nodes = input_nodes;
+			this->hidden_nodes = hidden_nodes;
+			this->output_nodes = output_nodes;
+
+			weights_ih.setSize(this->hidden_nodes, this->input_nodes);
+			weights_ho.setSize(this->output_nodes, this->hidden_nodes);
+			weights_ih.randomize();
+			weights_ho.randomize();
+
+
+			bias_h.setSize(this->hidden_nodes, 1);
+			bias_o.setSize(this->output_nodes, 1);
+			bias_h.randomize();
+			bias_o.randomize();
+
+			this->learning_rate = 0.1;
+		}
+
+		long double* predict(long double*, int);
+		void train(long double*, int, long double*, int);
+};
+
+long double* NeuralNetwork::predict(long double* input_array, int length) {
+	Matrix inputs = Matrix::fromArray(input_array, length);
+	Matrix hidden = (this->weights_ih) * inputs;
+	hidden = hidden + (this->bias_h);
+	// Activation Function
+	hidden = Matrix::map(hidden, 1);
+
+	Matrix output = (this->weights_ho) * hidden;
+	//output = Matrix::add(output, this->bias_o);
+	output = output + (this->bias_o);
+	// Activation Function
+	output = Matrix::map(output, 1);
+	output.print();
+
+	//return output;
+}
+
+void NeuralNetwork::train(long double* input_array, int input_length, long double* target_array, int target_length) {
+	Matrix inputs = Matrix::fromArray(input_array, input_length);
+	Matrix hidden = (this->weights_ih) * inputs;
+	hidden = hidden + (this->bias_h);
+	// Activation Function
+	hidden = Matrix::map(hidden, 1);
+
+	Matrix outputs = (this->weights_ho) * hidden;
+	outputs = outputs + (this->bias_o);
+	// Activation Function
+	outputs = Matrix::map(outputs, 1);
+
+	Matrix targets = Matrix::fromArray(target_array, target_length);
+
+	Matrix output_errors = targets - outputs;
+
+	// gradient = outputs * (1 - outputs);
+	// Calculate gradient
+
+	Matrix gradients = Matrix::map(outputs, 2);
+	gradients.dot(output_errors);
+	gradients.multiply(this->learning_rate);
+
+	// Calculate deltas
+	Matrix hidden_t = Matrix::transpose(hidden);
+	Matrix weight_ho_deltas = Matrix::multiply(gradients, hidden_t);
+	// Adjust the bais by its deltas
+	this->weights_ho = (this->weights_ho) + weight_ho_deltas;
+	this->bias_o = (this->bias_o) + gradients;
+
+	// Calculate hidden layer errors...
+	Matrix who_t = Matrix::transpose(this->weights_ho);
+	Matrix hidden_errors = who_t * output_errors;
+
+	// Calculate hidden layer gradient...
+	Matrix hidden_gradient = Matrix::map(hidden, 2);
+	hidden_gradient.dot(hidden_errors);
+	hidden_gradient.multiply(this->learning_rate);
+
+	// Calculate input->hidden deltas...
+	Matrix inputs_t = Matrix::transpose(inputs);
+	Matrix weight_ih_deltas = hidden_gradient * inputs_t;
+
+	// Adjust weights and bias...
+	this->weights_ih = (this->weights_ih) + weight_ih_deltas;
+	this->bias_h = (this->bias_h) + hidden_gradient;
+}
